625759680e418581ceef36036a01d8a560cad10c
Raul Sanchez <rsanchez@evopointsolution.com>
2021-02-18 00:00:50 -0400

    change

--- index.ts
+++ index.ts
@@ -3,18 +3,18 @@ import CsvReader from './implementation/csv_reader';
 import SmtpEmailSender from './implementation/smtp_email_sender'
 import SdkFonosProvider from './implementation/sdk_fonos_provider';
 
+require('dotenv').config()
 // Handling args
 var args = process.argv.slice(2);
 //End
-
 let config = {
     auth:{
-        user:'raulreyes23ss',
-        pass:'anonymousla23@',
+        user:process.env.AUTH_USER,
+        pass:process.env.AUTH_PASS,
     },
-    host:'smtp.gmail.com',
-    port:587,
-    secure:false
+    host:process.env.HOST,
+    port: parseInt(process.env.PORT),
+    secure: Boolean(process.env.SECURE)
 }
 let _processor = new Processor(new CsvReader(),new SmtpEmailSender(config), new SdkFonosProvider());
 _processor.run(args.toString()).then(result =>{
